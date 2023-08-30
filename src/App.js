ff383a97ca1bbf84d86c01a1e38d8ed594c1110a
Parsa Rostami <parsa84rostami@gmail.com>
2023-08-30 20:47:33 +0330

    coding Navbar and TopBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import routes from './routes';
+import { useRoutes } from 'react-router-dom'
+
 
 function App() {
+  const router = useRoutes(routes)
   return (
     <div className="App">
-
+      
+      {router}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/reset.css'
+import './styles/fonts.css'
+import './styles/variables.css'
+import './styles/helpers.css'
+import './styles/defaults.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
