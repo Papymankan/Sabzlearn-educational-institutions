2061730ef7611fb2c1ab1905e375adbacbcdba2d
Parsa Rostami <parsa84rostami@gmail.com>
2023-08-31 20:56:54 +0330

    Landing Header Added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,12 +2,14 @@ import React from 'react'
 import TopBar from './TopBar/TopBar'
 import './Header.css'
 import NavBar from './NavBar/NavBar'
+import Landing from './Landing/Landing'
 
 export default function Header() {
     return (
         <header class="header">
             <TopBar/>
             <NavBar/>
+            <Landing/>
         </header>
     )
 }
