35f82b36088c0e72f697c4af47d0f47fa4d89f29
Parsa Rostami <parsa84rostami@gmail.com>
2023-09-01 17:04:29 +0330

    coding last courses box logic

--- src/Components/LastCourses/LastCourses.js
+++ src/Components/LastCourses/LastCourses.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import './LastCourses.css'
 import SectionHeader from '../SectionHeader/SectionHeader'
+import CourseBox from '../CourseBox/CourseBox'
 
 export default function LastCourses() {
     return (
         <div className="courses">
             <div className="container">
                 <SectionHeader main_title="جدیدترین دوره ها" primary_title="سکوی پرتاب شما به سمت موفقیت" btn_title="تمامی دوره ها" />
+                <div class="courses-content">
+                    <div class="container">
+                        <div class="row">
+                            <CourseBox/>
+                            <CourseBox/>
+                            <CourseBox/>
+                            <CourseBox/>
+                            <CourseBox/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
