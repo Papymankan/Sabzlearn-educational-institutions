bfcfd8af991dc64493a947e59a49cc9c530561c0
Parsa Rostami <parsa84rostami@gmail.com>
2023-09-02 16:48:35 +0330

    coding breadcrumb for courseinfo page

--- src/Pages/CourseInfo/CourseInfo.js
+++ src/Pages/CourseInfo/CourseInfo.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import TopBar from './../../Components/Header/TopBar/TopBar'
+import NavBar from './../../Components/Header/NavBar/NavBar'
+import Footer from './../../Components/Footer/Footer'
+import BreadCrumb from '../../Components/BreadCrumb/BreadCrumb'
 
 export default function CourseInfo() {
   return (
-    <div>CourseInfo</div>
+    <>
+      <TopBar/>
+      <NavBar/>
+      <BreadCrumb
+        links={[
+          {id:1 , title:'خانه' , to:'/'},
+          {id:2 , title:'آموزش برنامه نویسی فرانت اند' , to:'/category/frontend'},
+          {id:3 , title:'دوره متخصص جاوا اسکریپت' , to:'/courseInfo/javascript'},
+        ]}
+      />
+
+      <Footer/>
+    </>
   )
 }
