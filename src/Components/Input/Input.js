bc1c1b1555006f1c3523ab0637562e78688e203c
Parsa Rostami <parsa84rostami@gmail.com>
2023-09-03 21:47:13 +0330

    coding reducer for input state managment

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import './Input.css'
+
+const inputReducer = (state , action) => {
+    switch (action.type){
+      case ('CHANGE') : {
+        return {...state , value:action.value , isValid:action.isValid}
+      }
+      default : {
+        return state
+      }
+    }
+}
 
 export default function Input(props) {
+  const [mainInput, dispatch] = useReducer(inputReducer, { value: '', isValid: false })
 
-    const InputOnChange = ()=>{
-
+  const InputOnChange = (e) => {
+    if(e.target.value){
+      dispatch({type:'CHANGE' , value : e.target.value , isValid:true})
+    }else{
+      dispatch({type:'CHANGE' , value : e.target.value , isValid:false})
     }
+  }
 
-    const element = props.element == 'input' ? (
-        <input placeholder={props.placeholder} type={props.type} className={props.classname} onChange={InputOnChange} />
-    ) : (
-        <textarea placeholder={props.placeholder} className={props.classname} onChange={InputOnChange}/>
-    )
+  const element = props.element == 'input' ? (
+    <input placeholder={props.placeholder} type={props.type} className={`${props.classname} ${mainInput.isValid ? 'success' : 'error'}`} onChange={InputOnChange} value={mainInput.value}/>
+  ) : (
+    <textarea placeholder={props.placeholder} className={`${props.classname} ${mainInput.isValid ? 'success' : 'error'}`} onChange={InputOnChange} value={mainInput.value}/>
+  )
 
   return (
     <div>
-        {element}
+      {element}
     </div>
   )
 }
