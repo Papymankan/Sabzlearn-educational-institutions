2e9dc253f636ef0838cd26d8ead2034bb7499a8e
Parsa Rostami <parsa84rostami@gmail.com>
2023-09-13 21:53:10 +0330

    fix courses category

--- frontend/src/Pages/Category/Category.js
+++ frontend/src/Pages/Category/Category.js
@@ -23,54 +23,64 @@ export default function Category() {
     <>
       <section class="courses">
         <div class="container">
-          <div class="courses-top-bar">
 
-            <div class="courses-top-bar__right">
-              <div class="courses-top-bar__row-btn courses-top-bar__icon--active">
-                <i class="fas fa-border-all courses-top-bar__icon"></i>
+          {
+            courses.length >= 1 ? (
+              <div class="courses-top-bar">
+
+                <div class="courses-top-bar__right">
+                  <div class="courses-top-bar__row-btn courses-top-bar__icon--active">
+                    <i class="fas fa-border-all courses-top-bar__icon"></i>
+                  </div>
+                  <div class="courses-top-bar__column-btn">
+                    <i class="fas fa-align-left courses-top-bar__icon"></i>
+                  </div>
+
+                  <div class="courses-top-bar__selection">
+                    <span class="courses-top-bar__selection-title">
+                      مرتب سازی پیش فرض
+                      <i class="fas fa-angle-down courses-top-bar__selection-icon"></i>
+                    </span>
+                    <ul class="courses-top-bar__selection-list">
+                      <li class="courses-top-bar__selection-item courses-top-bar__selection-item--active">مرتب سازی پیش فرض</li>
+                      <li class="courses-top-bar__selection-item">مربت سازی بر اساس محبوبیت</li>
+                      <li class="courses-top-bar__selection-item">مربت سازی بر اساس امتیاز</li>
+                      <li class="courses-top-bar__selection-item">مربت سازی بر اساس آخرین</li>
+                      <li class="courses-top-bar__selection-item">مربت سازی بر اساس ارزان ترین</li>
+                      <li class="courses-top-bar__selection-item">مربت سازی بر اساس گران ترین</li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div class="courses-top-bar__left">
+                  <form action="#" class="courses-top-bar__form">
+                    <input type="text" class="courses-top-bar__input" placeholder="جستجوی دوره ..." />
+                    <i class="fas fa-search courses-top-bar__search-icon"></i>
+                  </form>
+                </div>
+
               </div>
-              <div class="courses-top-bar__column-btn">
-                <i class="fas fa-align-left courses-top-bar__icon"></i>
-              </div>
 
-              <div class="courses-top-bar__selection">
-                <span class="courses-top-bar__selection-title">
-                  مرتب سازی پیش فرض
-                  <i class="fas fa-angle-down courses-top-bar__selection-icon"></i>
-                </span>
-                <ul class="courses-top-bar__selection-list">
-                  <li class="courses-top-bar__selection-item courses-top-bar__selection-item--active">مرتب سازی پیش فرض</li>
-                  <li class="courses-top-bar__selection-item">مربت سازی بر اساس محبوبیت</li>
-                  <li class="courses-top-bar__selection-item">مربت سازی بر اساس امتیاز</li>
-                  <li class="courses-top-bar__selection-item">مربت سازی بر اساس آخرین</li>
-                  <li class="courses-top-bar__selection-item">مربت سازی بر اساس ارزان ترین</li>
-                  <li class="courses-top-bar__selection-item">مربت سازی بر اساس گران ترین</li>
-                </ul>
-              </div>
-            </div>
-
-            <div class="courses-top-bar__left">
-              <form action="#" class="courses-top-bar__form">
-                <input type="text" class="courses-top-bar__input" placeholder="جستجوی دوره ..." />
-                <i class="fas fa-search courses-top-bar__search-icon"></i>
-              </form>
-            </div>
-
-          </div>
+            ) : null
+          }
 
           <div class="courses-content">
             <div class="container">
               <div className="row">
                 {
                   courses.length >= 1 ?
-                  courses.map(course => {
-                    return <CourseBox name={course.name} cover={course.cover} price={course.price} creator={course.creator} shortname={course.shortName}/>
-                  }) : <div className="alert alert-info">فعلا برای این بخش دوره ای در نظر گرفته نشده است</div>
+                    courses.map(course => {
+                      return <CourseBox name={course.name} cover={course.cover} price={course.price} creator={course.creator} shortname={course.shortName} />
+                    }) : <div className="alert alert-info">فعلا برای این بخش دوره ای در نظر گرفته نشده است</div>
                 }
               </div>
             </div>
           </div>
-          <Pagination />
+          {
+            courses.length >= 1 ? (
+              <Pagination />
+            ) : null
+          }
         </div>
       </section>
       <Footer />
