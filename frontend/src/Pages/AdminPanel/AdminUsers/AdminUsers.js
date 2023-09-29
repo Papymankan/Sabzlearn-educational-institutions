7fdaec85ffe825365d7d2286d4d0a79290ec54a9
Parsa Rostami <parsa84rostami@gmail.com>
2023-09-29 15:56:03 +0330

    coding delete user feature

--- frontend/src/Pages/AdminPanel/AdminUsers/AdminUsers.js
+++ frontend/src/Pages/AdminPanel/AdminUsers/AdminUsers.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import DataTable from '../../../Components/AdminPanel/DataTable/DataTable'
-
+import Swal from 'sweetalert2'
 export default function AdminUsers() {
+
   const [users, setUsers] = useState([])
+
   useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  const fetchUsers = () => {
     const localData = JSON.parse(localStorage.getItem('user'))
     fetch('http://localhost:4000/v1/users', {
       headers: {
@@ -15,7 +21,45 @@ export default function AdminUsers() {
         console.log(data)
         setUsers(data)
       })
-  }, [])
+  }
+
+  const deleteUser = (id) => {
+    const localData = JSON.parse(localStorage.getItem('user'))
+    Swal.fire({
+      title: '<p style="font-size: 30px ; margin-bottom: 10px;">آیا از حذف مطمئن هستید؟</p>',
+      icon: 'warning',
+      padding: '30px 0',
+      width: '400px',
+      showCancelButton: true,
+      cancelButtonText: 'نه',
+      confirmButtonText: 'بله'
+    }).then(res => {
+      if (res.isConfirmed) {
+        fetch(`http://localhost:4000/v1/users/${id}`, {
+          method: 'DELETE',
+          headers: {
+            'Authorization': `Bearer ${localData.token}`
+          }
+        }).then(res => {
+          if (res.ok) {
+            Swal.fire({
+              title: '<p style="font-size: 30px ; margin-bottom: 10px;">با موفقیت حذف شد</p>',
+              icon: 'success',
+              padding: '20px',
+              didOpen: () => {
+                Swal.showLoading()
+              },
+              width: '380px',
+              timer: 1500,
+              willClose: () => {
+                fetchUsers()
+              }
+            })
+          }
+        })
+      }
+    })
+  }
 
   return (
     <>
@@ -46,7 +90,7 @@ export default function AdminUsers() {
                     </button>
                   </td>
                   <td>
-                    <button type="button" class="btn btn-danger delete-btn">
+                    <button type="button" class="btn btn-danger delete-btn" onClick={() => deleteUser(user._id)}>
                       حذف
                     </button>
                   </td>
