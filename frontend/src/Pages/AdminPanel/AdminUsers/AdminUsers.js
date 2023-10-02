3bfea02d5f950ea34db5700e0a23a0d26370161b
Parsa Rostami <parsa84rostami@gmail.com>
2023-10-02 21:19:17 +0330

    add new user

--- frontend/src/Pages/AdminPanel/AdminUsers/AdminUsers.js
+++ frontend/src/Pages/AdminPanel/AdminUsers/AdminUsers.js
@@ -1,9 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import DataTable from '../../../Components/AdminPanel/DataTable/DataTable'
 import Swal from 'sweetalert2'
+import { emailValidator, maxValidator, minValidator, requiredValidator } from '../../../Validators/rules'
+import { useForm } from '../../../hooks/useForm'
+import Input from '../../../Components/Input/Input'
 export default function AdminUsers() {
 
   const [users, setUsers] = useState([])
+
+  const [formState, onInputHandler] = useForm({
+    name: {
+      value: '',
+      isValid: false
+    },
+    username: {
+      value: '',
+      isValid: false
+    },
+    email: {
+      value: '',
+      isValid: false
+    },
+    password: {
+      value: '',
+      isValid: false
+    },
+    phone: {
+      value: '',
+      isValid: false
+    }
+  }
+    , false)
+
 
   useEffect(() => {
     fetchUsers()
@@ -97,10 +125,143 @@ export default function AdminUsers() {
     })
   }
 
-
+  const registerNewUser = (event) => {
+    event.preventDefault();
+    let newUser = {
+      name: formState.inputs.name.value,
+      username: formState.inputs.username.value,
+      phone: formState.inputs.phone.value,
+      email: formState.inputs.email.value,
+      password: formState.inputs.password.value,
+      confirmPassword: formState.inputs.password.value
+    }
+    fetch('http://localhost:4000/v1/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    }).then(res => {
+      console.log(res);
+      res.json()
+    }).then(result => {
+      console.log(result);
+      fetchUsers()
+    })
+  }
 
   return (
     <>
+      <div class="home-content-edit">
+        <div class="back-btn">
+          <i class="fas fa-arrow-right"></i>
+        </div>
+        <form class="form">
+          <div class="col-6">
+            <div class="name input">
+              <label class="input-title">نام و نام خانوادگی</label>
+              <Input
+                type="text"
+                className=""
+                id="name"
+                element="input"
+                validation={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا نام و نام خانوادگی کاربر را وارد کنید..."
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="family input">
+              <label class="input-title">نام کاربری</label>
+              <Input
+                type="text"
+                className=""
+                id="username"
+                element="input"
+                validation={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا نام کاربری را وارد کنید..."
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="email input">
+              <label class="input-title">ایمیل</label>
+              <Input
+                type="text"
+                className=""
+                id="email"
+                element="input"
+                validation={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                  emailValidator(),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا ایمیل کاربر را وارد کنید..."
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="password input">
+              <label class="input-title">رمز عبور</label>
+              <Input
+                type="text"
+                className=""
+                id="password"
+                element="input"
+                validation={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا رمز عبور کاربر را وارد کنید..."
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="phone input">
+              <label class="input-title">شماره تلفن</label>
+              <Input
+                type="text"
+                className=""
+                id="phone"
+                element="input"
+                validation={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا شماره تلفن کاربر را وارد کنید..."
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="bottom-form">
+              <div class="submit-btn">
+                <input type="submit" value="افزودن" onClick={registerNewUser} />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
       <DataTable title="کاربران">
         <table class="table">
           <thead>
