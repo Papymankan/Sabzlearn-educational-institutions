94fd271eac849abd03fe1f4ee639a922c26bdee0
Parsa Rostami <parsa84rostami@gmail.com>
2023-10-05 19:17:56 +0330

    coding add new category ui

--- frontend/src/Pages/AdminPanel/AdminCategory/AdminCategory.js
+++ frontend/src/Pages/AdminPanel/AdminCategory/AdminCategory.js
@@ -1,18 +1,88 @@
 import React, { useEffect, useState } from 'react'
+import './AdminCategory.css'
 import { Link } from 'react-router-dom'
 import DataTable from '../../../Components/AdminPanel/DataTable/DataTable'
+import Input from '../../../Components/Input/Input'
+import { useForm } from '../../../hooks/useForm'
+import { emailValidator, maxValidator, minValidator, requiredValidator } from '../../../Validators/rules'
 
 export default function AdminCategory() {
-    const [categories , setCategories] = useState([])
-    useEffect( ()=>{
-        fetch('http://localhost:4000/v1/category').then(res => res.json()).then(data =>{
+    const [categories, setCategories] = useState([])
+
+    const [formState , onInputHandler] = useForm ( {
+        title : {
+            value : '',
+            isValid : false
+        },
+        shortname : {
+            value : '',
+            isValid : false
+        },
+    } , false)
+
+    useEffect(() => {
+        fetch('http://localhost:4000/v1/category').then(res => res.json()).then(data => {
             setCategories(data)
             console.log(data);
-        } )
+        })
     }, [])
 
     return (
         <>
+            <div class="container-fluid" id="home-content">
+                <div class="container">
+                    <div class="home-title">
+                        <span>افزودن دسته‌بندی جدید</span>
+                    </div>
+                    <form class="form">
+                        <div class="col-6">
+                            <div class="name input">
+                                <label class="input-title">عنوان</label>
+                                <Input
+                                    element="input"
+                                    onInputHandler={onInputHandler}
+                                    type="text"
+                                    id="title"
+                                    placeholder="لطفا عنوان را وارد کنید..."
+                                    validation={[
+                                        requiredValidator(),
+                                        minValidator(8),
+                                        maxValidator(20),
+                                      ]}                                />
+                                <span class="error-message text-danger"></span>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="name input">
+                                <label class="input-title">اسم کوتاه</label>
+                                <Input
+                                    element="input"
+                                    onInputHandler={onInputHandler}
+                                    type="text"
+                                    id="shortname"
+                                    placeholder="لطفا اسم کوتاه را وارد کنید..."
+                                    validation={[
+                                        requiredValidator(),
+                                        minValidator(8),
+                                        maxValidator(20),
+                                      ]}
+                                />
+                                <span class="error-message text-danger"></span>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="bottom-form">
+                                <div class="submit-btn">
+                                    <input
+                                        type="submit"
+                                        value="افزودن"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
             <DataTable title={'دسته بندی ها'}>
                 <table class="table">
                     <thead>
@@ -24,28 +94,28 @@ export default function AdminCategory() {
                         </tr>
                     </thead>
                     <tbody>
-                            {
-                                categories.map((category, index) => (
-                                    <tr>
-                                        <td>{index + 1}</td>
-                                        <td>
-                                            <Link to={`/category/${category.name}/1`} target='_blank'>
-                                                {category.title}
-                                            </Link>
-                                        </td>
-                                        <td>
-                                            <button type="button" class="btn btn-primary edit-btn">
-                                                ویرایش
-                                            </button>
-                                        </td>
-                                        <td>
-                                            <button type="button" class="btn btn-danger delete-btn" >
-                                                حذف
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))
-                            }
+                        {
+                            categories.map((category, index) => (
+                                <tr>
+                                    <td>{index + 1}</td>
+                                    <td>
+                                        <Link to={`/category/${category.name}/1`} target='_blank'>
+                                            {category.title}
+                                        </Link>
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-primary edit-btn">
+                                            ویرایش
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-danger delete-btn" >
+                                            حذف
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
                     </tbody>
                 </table>
             </DataTable>
