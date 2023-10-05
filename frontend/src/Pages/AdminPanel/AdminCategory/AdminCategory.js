5c3bbb68a03b7c443400f3925de62a6b8e37859a
Parsa Rostami <parsa84rostami@gmail.com>
2023-10-05 18:30:47 +0330

    fetch categories info in to the table

--- frontend/src/Pages/AdminPanel/AdminCategory/AdminCategory.js
+++ frontend/src/Pages/AdminPanel/AdminCategory/AdminCategory.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import DataTable from '../../../Components/AdminPanel/DataTable/DataTable'
 
 export default function AdminCategory() {
+    const [categories , setCategories] = useState([])
+    useEffect( ()=>{
+        fetch('http://localhost:4000/v1/category').then(res => res.json()).then(data =>{
+            setCategories(data)
+            console.log(data);
+        } )
+    }, [])
+
     return (
         <>
             <DataTable title={'دسته بندی ها'}>
@@ -10,16 +19,33 @@ export default function AdminCategory() {
                         <tr>
                             <th>شناسه</th>
                             <th>عنوان</th>
-                            <th>مبلغ</th>
-                            <th>وضعیت</th>
-                            <th>مدرس</th>
-                            <th>دسته بندی</th>
                             <th>ویرایش</th>
                             <th>حذف</th>
                         </tr>
                     </thead>
                     <tbody>
-                        
+                            {
+                                categories.map((category, index) => (
+                                    <tr>
+                                        <td>{index + 1}</td>
+                                        <td>
+                                            <Link to={`/category/${category.name}/1`} target='_blank'>
+                                                {category.title}
+                                            </Link>
+                                        </td>
+                                        <td>
+                                            <button type="button" class="btn btn-primary edit-btn">
+                                                ویرایش
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <button type="button" class="btn btn-danger delete-btn" >
+                                                حذف
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
                     </tbody>
                 </table>
             </DataTable>
