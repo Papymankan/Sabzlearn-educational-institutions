b1ccc4ea7be7c212b01a0ea25e7c925a16213261
Parsa Rostami <parsa84rostami@gmail.com>
2023-10-20 19:54:07 +0330

    coding answer contact

--- frontend/src/Pages/AdminPanel/AdminContact/AdminContact.js
+++ frontend/src/Pages/AdminPanel/AdminContact/AdminContact.js
@@ -17,13 +17,73 @@ export default function AdminContact() {
         fetchContacts()
     }, [])
 
-    const showContact = (body)=>{
+    const showContact = (body) => {
         Swal.fire({
             title: `<p style="font-size: 18px ; margin-bottom: 10px;">${body}</p>`,
             // icon: 'warning',
             padding: '30px 0',
             width: '400px',
             confirmButtonText: 'بستن'
+        })
+    }
+
+    const AnswerContact = (email) => {
+        Swal.fire({
+            title: '<p style="font-size: 30px ; margin-bottom: 10px;">پاسخ را وارد کنید</p>',
+            html:
+                '<textArea id="swal-input1" class="swal2-input" placeholder="پاسخ">',
+            padding: '5px',
+            width: '380px',
+            preConfirm: () => {
+                return [
+                    document.getElementById('swal-input1').value,
+                ]
+            },
+            confirmButtonText: 'تایید',
+        }).then(res => {
+            if (res.isConfirmed && res.value[0]) {
+
+                let answer = {
+                    email: email,
+                    answer: res.value[0]
+                }
+                const localData = JSON.parse(localStorage.getItem('user'))
+                fetch('http://localhost:4000/v1/contact/answer', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localData.token}`,
+                    },
+                    body: JSON.stringify(answer)
+                }).then(res => {
+                    console.log('res.ok1');
+                    if (res.ok) {
+                        console.log('res.ok2');
+                        Swal.fire({
+                            title: '<p style="font-size: 30px ; margin-bottom: 10px;">با موفقیت پاسخ داده شد</p>',
+                            icon: 'success',
+                            padding: '20px',
+                            didOpen: () => {
+                                Swal.showLoading()
+                            },
+                            width: '380px',
+                            timer: 1500,
+                        })
+                        fetchContacts()
+                    } else {
+                        Swal.fire({
+                            title: '<p style="font-size: 30px ; margin-bottom: 10px;">با موفقیت پاسخ داده نشد</p>',
+                            icon: 'error',
+                            padding: '20px',
+                            didOpen: () => {
+                                Swal.showLoading()
+                            },
+                            width: '380px',
+                            timer: 1500,
+                        })
+                    }
+                })
+            }
         })
     }
 
@@ -52,7 +112,7 @@ export default function AdminContact() {
                                         <td>{contact.email}</td>
                                         <td>{contact.phone}</td>
                                         <td>
-                                            <button type="button" class="btn btn-primary edit-btn" onClick={()=>showContact(contact.body)}>
+                                            <button type="button" class="btn btn-primary edit-btn" onClick={() => showContact(contact.body)}>
                                                 مشاهده
                                             </button>
                                         </td>
@@ -62,8 +122,8 @@ export default function AdminContact() {
                                             </button>
                                         </td>
                                         <td>
-                                            <button type="button" class="btn btn-success delete-btn">
-                                                پاسخ
+                                            <button type="button" class="btn btn-success delete-btn" onClick={() => AnswerContact(contact.email)} disabled={contact.answer}>
+                                                {contact.answer ? 'پاسخ داده شده' : 'پاسخ'}
                                             </button>
                                         </td>
                                     </tr>
