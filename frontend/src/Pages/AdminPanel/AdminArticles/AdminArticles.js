66a4ccf9dcbf75da87cac4b05a9b71ef1378b519
Parsa Rostami <parsa84rostami@gmail.com>
2023-10-28 14:59:36 +0330

    coding delete articles

--- frontend/src/Pages/AdminPanel/AdminArticles/AdminArticles.js
+++ frontend/src/Pages/AdminPanel/AdminArticles/AdminArticles.js
@@ -1,26 +1,64 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import Swal from 'sweetalert2'
 import DataTable from '../../../Components/AdminPanel/DataTable/DataTable'
 
 export default function AdminArticles() {
 
-    const [articles , setArticles] = useState([])
+    const [articles, setArticles] = useState([])
 
-    const fetchArticles = ()=>{
+    const fetchArticles = () => {
         fetch('http://localhost:4000/v1/articles')
-        .then(res => res.json())
-        .then(data => setArticles(data))
+            .then(res => res.json())
+            .then(data => setArticles(data))
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchArticles()
-    } , [])
+    }, [])
 
-  return (
-    
-    <>
-    <DataTable title={'مقاله ها'}>
-    <table class="table">
+    const DeleteArticle = (id)=>{
+        Swal.fire({
+            title: '<p style="font-size: 30px ; margin-bottom: 10px;">آیا از حذف مطمئن هستید؟</p>',
+            icon: 'warning',
+            padding: '30px 0',
+            width: '400px',
+            showCancelButton: true,
+            cancelButtonText: 'نه',
+            confirmButtonText: 'بله'
+        }).then(res => {
+            if (res.isConfirmed) {
+                const localData = JSON.parse(localStorage.getItem('user'))
+                fetch(`http://localhost:4000/v1/articles/${id}` , {
+                    method:'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${localData.token}`,
+                    },
+                }).then(res => {
+                    if(res.ok){
+                        Swal.fire({
+                            title: '<p style="font-size: 30px ; margin-bottom: 10px;">با موفقیت حذف شد</p>',
+                            icon: 'success',
+                            padding: '20px',
+                            didOpen: () => {
+                                Swal.showLoading()
+                            },
+                            width: '380px',
+                            timer: 1500,
+                        })
+                        fetchArticles()
+                    }
+                })
+            }
+        })
+    }
+
+
+    return (
+
+        <>
+            <DataTable title={'مقاله ها'}>
+                <table class="table">
                     <thead>
                         <tr>
                             <th>شناسه</th>
@@ -33,7 +71,7 @@ export default function AdminArticles() {
                     </thead>
                     <tbody>
                         {
-                            articles.map((article , index)=> (
+                            articles.map((article, index) => (
                                 <tr>
                                     <td>{index + 1}</td>
                                     <td>
@@ -53,7 +91,7 @@ export default function AdminArticles() {
                                         </button>
                                     </td>
                                     <td>
-                                        <button type="button" class="btn btn-danger delete-btn">
+                                        <button type="button" class="btn btn-danger delete-btn" onClick={() => DeleteArticle(article._id)}>
                                             حذف
                                         </button>
                                     </td>
@@ -62,7 +100,7 @@ export default function AdminArticles() {
                         }
                     </tbody>
                 </table>
-    </DataTable>
-    </>
-  )
+            </DataTable>
+        </>
+    )
 }
