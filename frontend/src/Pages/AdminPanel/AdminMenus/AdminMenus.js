5d0f6d630118996ee4fcfe7af526a0084d0ee347
Parsa Rostami <parsa84rostami@gmail.com>
2023-11-24 17:39:34 +0330

    remove menu

--- frontend/src/Pages/AdminPanel/AdminMenus/AdminMenus.js
+++ frontend/src/Pages/AdminPanel/AdminMenus/AdminMenus.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import Swal from 'sweetalert2'
 import DataTable from '../../../Components/AdminPanel/DataTable/DataTable'
 
 
@@ -7,9 +8,52 @@ export default function AdminMenus() {
 
     const [menus, setMenus] = useState([])
 
-    useEffect(() => {
+    const fetchMenus= ()=>{
         fetch('http://localhost:4000/v1/menus/all').then(res => res.json()).then(data => setMenus(data))
+    }
+
+    useEffect(() => {
+        fetchMenus()
     }, [])
+
+    const deleteMenu = (id) => {
+        Swal.fire({
+            title: '<p style="font-size: 30px ; margin-bottom: 10px;">آیا از حذف مطمئن هستید؟</p>',
+            icon: 'warning',
+            padding: '30px 0',
+            width: '400px',
+            showCancelButton: true,
+            cancelButtonText: 'نه',
+            confirmButtonText: 'بله'
+        }).then(res => {
+            if (res.isConfirmed) {
+                const localData = JSON.parse(localStorage.getItem('user'))
+                fetch(`http://localhost:4000/v1/menus/${id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${localData.token}`
+                    }
+                }).then(res => {
+                    if(res.ok){
+                        Swal.fire({
+                            title: '<p style="font-size: 30px ; margin-bottom: 10px;">با موفقیت حذف شد</p>',
+                            icon: 'success',
+                            padding: '20px',
+                            didOpen: () => {
+                                Swal.showLoading()
+                            },
+                            width: '380px',
+                            timer: 1500,
+                        })
+                    }
+                    fetchMenus()
+                })
+            }
+        })
+
+
+
+    }
 
     return (
         <>
@@ -43,7 +87,7 @@ export default function AdminMenus() {
                                         </button>
                                     </td>
                                     <td>
-                                        <button type="button" class="btn btn-danger delete-btn">
+                                        <button type="button" class="btn btn-danger delete-btn" onClick={() => deleteMenu(menu._id)}>
                                             حذف
                                         </button>
                                     </td>
