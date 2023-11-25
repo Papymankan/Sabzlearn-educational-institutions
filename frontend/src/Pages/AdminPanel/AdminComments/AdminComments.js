8865b2cb5a8c3a5acccf0728268efe02115864b9
Parsa Rostami <parsa84rostami@gmail.com>
2023-11-25 12:36:10 +0330

    coding ban user for comment

--- frontend/src/Pages/AdminPanel/AdminComments/AdminComments.js
+++ frontend/src/Pages/AdminPanel/AdminComments/AdminComments.js
@@ -60,6 +60,43 @@ export default function AdminComments() {
         })
     }
 
+    const BanUser = (id) => {
+        Swal.fire({
+            title: '<p style="font-size: 30px ; margin-bottom: 10px;">آیا از بن مطمئن هستید؟</p>',
+            icon: 'warning',
+            padding: '30px 0',
+            width: '400px',
+            showCancelButton: true,
+            cancelButtonText: 'نه',
+            confirmButtonText: 'بله'
+        }).then(res => {
+            if (res.isConfirmed) {
+                const localData = JSON.parse(localStorage.getItem('user'))
+                fetch(`http://localhost:4000/v1/users/ban/${id}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Authorization': `Bearer ${localData.token}`,
+                    }
+                }).then(res=>{
+                    if(res.ok){
+                        Swal.fire({
+                            title: '<p style="font-size: 30px ; margin-bottom: 10px;">با موفقیت بن شد</p>',
+                            icon: 'success',
+                            padding: '20px',
+                            didOpen: () => {
+                                Swal.showLoading()
+                            },
+                            width: '380px',
+                            timer: 1500,
+                        })
+                        fetchComments()
+                    }
+                })
+            }
+        })
+
+    }
+
     return (
         <>
             <DataTable title={'کامنت ها'}>
@@ -70,9 +107,9 @@ export default function AdminComments() {
                             <th>ثبت کننده</th>
                             <th>دوره</th>
                             <th>پیام</th>
-                            <th>نمایش</th>
                             <th>پاسخ</th>
                             <th>حذف</th>
+                            <th>بن</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -95,6 +132,11 @@ export default function AdminComments() {
                                     <td>
                                         <button type="button" class="btn btn-danger delete-btn" onClick={() => DeleteComment(comment._id)}>
                                             حذف
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-danger delete-btn" onClick={() => BanUser(comment.creator._id)}>
+                                            بن
                                         </button>
                                     </td>
                                 </tr>
