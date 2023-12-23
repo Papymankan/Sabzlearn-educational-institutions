26693c2aa1b6d8314ba342012ae48c20e8b3a8f6
Parsa Rostami <parsa84rostami@gmail.com>
2023-12-23 12:01:26 +0330

    show courses by tab

--- frontend/src/Pages/UserPanel/UserCourses/UserCourses.js
+++ frontend/src/Pages/UserPanel/UserCourses/UserCourses.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import './UserCourses.css'
 
 export default function UserCourses() {
     const [courses, setCourses] = useState([])
+    const [showCourses, setShowCourses] = useState([])
+
+    const [tab , setTab] = useState('all')
 
     useEffect(() => {
         const localData = JSON.parse(localStorage.getItem('user'))
@@ -13,36 +17,56 @@ export default function UserCourses() {
         }).then(res => res.json()).then(data => setCourses(data))
     }, [])
 
+    const FilterCourses = ()=>{
+        if(tab == 'all'){
+            setShowCourses(courses)
+        }else if(tab == 'free'){
+            let arr = courses.filter(course => course.course.price == 0)
+            setShowCourses(arr)
+        }else if(tab == 'notFree'){
+            let arr = courses.filter(course => course.course.price != 0)
+            setShowCourses(arr)
+        }
+        
+    }
+
+    useEffect(FilterCourses , [tab])
+
     return (
         <div class="col-9">
             <div class="courses">
-                <div class="courses-header">
+                <div class="Usercourses-header">
                     <span class="courses-header__title">دوره های ثبت نام شده</span>
                     <ul class="courses-header__list">
                         <li class="courses-header__item">
-                            <a
-                                class="courses-header__link courses-header__link-active"
-                                href="#"
+                            <a class={`Usercourses-header__link  ${tab == 'all'?'Usercourses-header__link-active' : ''}`}
+                                href="#" onClick={(e)=>{
+                                    e.preventDefault()
+                                    setTab('all')}}
                             >
                                 همه دوره ها
                             </a>
                         </li>
                         <li class="courses-header__item">
-                            <a class="courses-header__link" href="#">
-                                دوره های فعال
+                            <a class={`Usercourses-header__link  ${tab == 'free'?'Usercourses-header__link-active' : ''}`} href="#" onClick={(e)=>{
+                                    e.preventDefault()
+                                    setTab('free')}}>
+                                دوره های رایگان
                             </a>
                         </li>
                         <li class="courses-header__item">
-                            <a class="courses-header__link" href="#">
-                                دوره های تکمیل شده
-                            </a>
+                            <a class={`Usercourses-header__link  ${tab == 'notFree'?'Usercourses-header__link-active' : ''}`} href="#" onClick={(e)=>{
+                                    e.preventDefault()
+                                    setTab('notFree')}}>
+                                دوره های پولی
+                            </a>    
                         </li>
                     </ul>
                 </div>
-                <div class="main">
+                <div class="UserCourses-main">
                     <div class="row">
                         <div class="col-12">
-                            {courses.map((course) => (
+                            {showCourses.map((course) => (
                                 <div class="main__box">
                                     <div class="main__box-right">
                                         <a class="main__box-img-link" href="#">
@@ -53,19 +77,19 @@ export default function UserCourses() {
                                         </a>
                                     </div>
                                     <div class="main__box-left">
-                                        <a href="#" class="main__box-title">
+                                        <Link to={`/course-info/${course.course.shortName}`} class="main__box-title">
                                             {course.course.name}
-                                        </a>
+                                        </Link>
                                         <div class="main__box-bottom">
                                             <div class="main__box-all">
-                                                <span class="main__box-all-text">کل دروس:</span>
-                                                <span class="main__box-all-value">75</span>
+                                                <span class="main__box-all-text">وضعیت:</span>
+                                                <span class="main__box-all-value">{course.course.isComplete ? 'تکمیل شده' : ' در حال برگزاری'}</span>
                                             </div>
                                             <div class="main__box-completed">
                                                 <span class="main__box-completed-text">
-                                                    دروس تکمیل شده:
+                                                    قیمت:{' '}
                                                 </span>
-                                                <span class="main__box-completed-value">0/75</span>
+                                                <span class="main__box-completed-value">{course.course.price ? course.course.price.toLocaleString() : 'رایگان'}</span>
                                             </div>
                                         </div>
                                     </div>
