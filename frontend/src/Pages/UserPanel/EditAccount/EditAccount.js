511daa5ceb249f5e614f498f6c18ecf2ad64458c
Parsa Rostami <parsa84rostami@gmail.com>
2023-12-30 11:49:39 +0330

    update user info

--- frontend/src/Pages/UserPanel/EditAccount/EditAccount.js
+++ frontend/src/Pages/UserPanel/EditAccount/EditAccount.js
@@ -1,24 +1,65 @@
 import React, { useContext, useEffect, useState } from "react";
 import './EditAccount.css'
 import AuthContext from "../../../Context/authContext";
+import { useNavigate } from "react-router";
+import Swal from "sweetalert2";
 
 export default function EditAccount() {
 
     const authContext = useContext(AuthContext)
-    console.log(authContext);
 
-    const [phone , setPhone] = useState()
-    const [name , setName] = useState()
-    const [userName , setUserName] = useState()
-    const [email , setEmail] = useState()
+    const navigate = useNavigate()
+
+    const [phone, setPhone] = useState('')
+    const [name, setName] = useState('')
+    const [username, setUserName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
 
-    useEffect(()=>{
-        setPhone(authContext.userInfos.phone)
+    useEffect(() => {
+        setPhone(authContext.userInfos ? authContext.userInfos.phone : '')
         setName(authContext.userInfos.name)
         setUserName(authContext.userInfos.username)
         setEmail(authContext.userInfos.email)
-    } , [authContext])
+    }, [authContext])
+
+    const updateUser = (e) => {
+        e.preventDefault()
+        let user = {
+            phone,
+            name,
+            username,
+            email,
+            password,
+        }
+        const localData = JSON.parse(localStorage.getItem('user'))
+        fetch('http://localhost:4000/v1/users', {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${localData.token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        }).then(res => {
+            if (res.ok) {
+                Swal.fire({
+                    title: '<p style="font-size: 30px ; margin-bottom: 10px;">با موفقیت ذخیره شد</p>',
+                    icon: 'success',
+                    padding: '20px',
+                    didOpen: () => {
+                        Swal.showLoading()
+                    },
+                    width: '380px',
+                    timer: 1500,
+                    willClose: () => {
+                        navigate('/user-panel', { replace: true })
+                        window.location.reload()
+                    }
+                })
+            }
+        })
+    }
 
     return (
         <>
@@ -31,11 +72,17 @@ export default function EditAccount() {
                                     <label class="edit__label">شماره موبایل *</label>
                                     <input
                                         class="edit__input"
-                                        type="text"
+                                        type="number"
                                         value={phone}
                                         onChange={event => setPhone(event.target.value)}
                                         placeholder="لطفا شماره موبایل خود را وارد کنید"
                                     />
+                                    {Boolean(phone) && phone.length != 11 && <span class="phone__help">
+                                        شماره تماس معتبر نیست
+                                    </span>}
+                                    {
+                                        console.log(typeof (phone))
+                                    }
                                 </div>
 
                                 <div class="col-12">
@@ -53,7 +100,7 @@ export default function EditAccount() {
                                     <input
                                         class="edit__input"
                                         type="text"
-                                        value={userName}
+                                        value={username}
                                         onChange={event => setUserName(event.target.value)}
                                         placeholder="لطفا نام نمایشی خود را وارد کنید"
                                     />
@@ -65,11 +112,16 @@ export default function EditAccount() {
                                     <label class="edit__label">آدرس ایمیل *</label>
                                     <input
                                         class="edit__input"
-                                        type="text"
+                                        type="email"
                                         value={email}
                                         onChange={event => setEmail(event.target.value)}
                                         placeholder="لطفا نام نمایشی خود را وارد کنید"
                                     />
+                                    {
+                                        !email.includes('@') && <span class="phone__help">
+                                            ایمیل معتبر نیست
+                                        </span>
+                                    }
                                 </div>
                             </div>
                         </div>
@@ -77,36 +129,23 @@ export default function EditAccount() {
                             <span class="edit__password-title">تغییر گذرواژه</span>
                             <div class="row">
                                 <div class="col-12">
-                                    <label class="edit__label">
-                                        گذرواژه پیشین (در صورتی که قصد تغییر ندارید خالی بگذارید)
-                                    </label>
+                                    <label class="edit__label"> گذرواژه جدید</label>
                                     <input
                                         class="edit__input"
                                         type="text"
-                                        placeholder="گذرواژه پیشین"
+                                        value={password}
+                                        onChange={event => setPassword(event.target.value)}
+                                        placeholder=" گذرواژه جدید"
                                     />
-                                </div>
-                                <div class="col-12">
-                                    <label class="edit__label">
-                                        گذرواژه جدید (در صورتی که قصد تغییر ندارید خالی بگذارید)
-                                    </label>
-                                    <input
-                                        class="edit__input"
-                                        type="text"
-                                        placeholder="گذرواژه جدید"
-                                    />
-                                </div>
-                                <div class="col-12">
-                                    <label class="edit__label">تکرار گذرواژه جدید</label>
-                                    <input
-                                        class="edit__input"
-                                        type="text"
-                                        placeholder="تکرار گذرواژه جدید"
-                                    />
+                                    {
+                                        password.length < 8 && <span class="phone__help">
+                                        حداقل 8 کاراکتر باشد
+                                    </span>
+                                    }
                                 </div>
                             </div>
                         </div>
-                        <button class="edit__btn" type="submit">
+                        <button class="edit__btn" type="submit" onClick={updateUser} disabled={!name || phone.length != 11 || !email.includes('@') || password.length < 8 || !username}>
                             ذخیره تغییرات
                         </button>
                     </form>
